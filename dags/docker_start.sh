79b0df4e24671d6c30fe1977fce5683344d28adc
linzichao <joy10393@gmail.com>
2020-07-31 18:33:23 +0800

    add docker-compose

--- dags/docker_start.sh
+++ dags/docker_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python3 ./crawler_dag.py
 python3 ./cleanup_dag.py
+celery worker -A celery_config --loglevel=info > celery_worker.log 2> celery_worker_err.log &
+
 airflow initdb
 
 cd ..
@@ -8,6 +10,5 @@ cp -r dags /root/airflow/
 cp .env /root/airflow/dags/
 airflow webserver -p 8080 > airflow_web.log 2> airflow_web_err.log &
 airflow scheduler > airflow_scheduler.log 2> airflow_scheduler_err.log &
-celery worker -A celery_config --loglevel=info > celery_worker.log 2> celery_worker_err.log &
 
 python3 -m flask run --host=0.0.0.0
